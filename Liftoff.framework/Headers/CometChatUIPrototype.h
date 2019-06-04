73393dfeb3cb4f845acb3b85a2f5c8ca9cfc79cb
Jeet Kapadia <jeet.kapadia@inscripts.in>
2019-06-04 22:08:48 +0530

    First release

---
+++ Liftoff.framework/Headers/CometChatUIPrototype.h
@@ -0,0 +1,17 @@
+//
+//  CometChatUIPrototype.h
+//  CometChatUIPrototype
+//
+//  Created by Jeet on 14/02/19.
+//  Copyright © 2019 Inscripts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CometChatUIPrototype.
+FOUNDATION_EXPORT double CometChatUIPrototypeVersionNumber;
+
+//! Project version string for CometChatUIPrototype.
+FOUNDATION_EXPORT const unsigned char CometChatUIPrototypeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CometChatUIPrototype/PublicHeader.h>
---
+++ LiftoffEngine.framework/Headers/CometChatGoEnginePrototype.h
@@ -0,0 +1,19 @@
+//
+//  CometChatGoEnginePrototype.h
+//  CometChatGoEnginePrototype
+//
+//  Created by Jeet on 14/02/19.
+//  Copyright © 2019 Inscripts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CometChatGoEnginePrototype.
+FOUNDATION_EXPORT double CometChatGoEnginePrototypeVersionNumber;
+
+//! Project version string for CometChatGoEnginePrototype.
+FOUNDATION_EXPORT const unsigned char CometChatGoEnginePrototypeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CometChatGoEnginePrototype/PublicHeader.h>
+
+#import "FMDB.h"
